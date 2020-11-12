3baeaf7e6ac1ed2f0168006d928bc916e1146481
Ellie Lambert <ebezjian@gmail.com>
2020-11-11 21:48:30 -0500

    add a bunch of crap

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 //user logic
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import pizzaCalc from './src/second.js';
 
 
-// $(document).ready(function() {
-//   $('TODO').submit(function(event) {
-//     event.preventDefault();
-//   });
-// });
+$(document).ready(function() {
+  $('form.orderForm').submit(function(event) {
+    event.preventDefault();
+    const sizesOptions = $("#sizes").find(":selected").text();
+    const toppingsOptions = $("#toppings").find(":selected").text();
+    const orderAmount = $("#amount").val()
+    const newPizza = new Pizza (sizesOptions, toppingsOptions, orderAmount)
+
+    const grandTotal = $("#totalCost").val()
+    const currentAmount = Pizza.pizzaCalc()
+  });
+});
--- src/second.js
+++ src/second.js
@@ -1,4 +1,33 @@
 //business logic
-// export default function TODO(){
+function pizzaCalc(toppings,sizes,cost){
+  this.sizes= sizes
+  this.toppings= toppings
+  this.cost= cost
+}
 
-// }
+pizzaCalc.prototype.Pizzatime = function(){
+  let cost=0
+  if(this.sizes === 'Small'){
+    cost += 2
+  }
+  if(this.sizes === 'Medium'){
+    cost += 3
+  }
+  if(this.sizes === 'Large'){
+    cost += 10
+  }
+  if(this.toppings ==='none'){
+    cost += 0
+  }
+  if(this.toppings === 'Chicken'){
+    cost += 1
+  }
+  if(this.toppings === 'Pepperoni'){
+    cost += 1
+  }
+  if (this.toppings === 'Steak'){
+    cost += 2
+  }
+  return cost
+}
+
